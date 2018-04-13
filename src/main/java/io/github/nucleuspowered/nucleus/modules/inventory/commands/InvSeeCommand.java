6cb4c58b9ba0f5c5d24287c09aa91c9792b3d8b2
Mohron <mohron@mohron.net>
2018-04-13 12:30:19 -0500

    Added viewable equipment inventory to invsee

--- src/main/java/io/github/nucleuspowered/nucleus/modules/inventory/commands/InvSeeCommand.java
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/inventory/commands/InvSeeCommand.java
@@ -4,6 +4,7 @@
  */
 package io.github.nucleuspowered.nucleus.modules.inventory.commands;
 
+import io.github.nucleuspowered.nucleus.Nucleus;
 import io.github.nucleuspowered.nucleus.argumentparsers.NicknameArgument;
 import io.github.nucleuspowered.nucleus.argumentparsers.SelectorWrapperArgument;
 import io.github.nucleuspowered.nucleus.internal.annotations.Since;
@@ -25,8 +26,25 @@ import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.args.CommandElement;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.entity.living.player.User;
+import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.item.inventory.Container;
 import org.spongepowered.api.item.inventory.Inventory;
+import org.spongepowered.api.item.inventory.InventoryArchetype;
+import org.spongepowered.api.item.inventory.InventoryArchetypes;
+import org.spongepowered.api.item.inventory.ItemStack;
+import org.spongepowered.api.item.inventory.Slot;
+import org.spongepowered.api.item.inventory.entity.Hotbar;
+import org.spongepowered.api.item.inventory.entity.MainPlayerInventory;
+import org.spongepowered.api.item.inventory.entity.PlayerInventory;
+import org.spongepowered.api.item.inventory.entity.UserInventory;
+import org.spongepowered.api.item.inventory.equipment.EquipmentType;
+import org.spongepowered.api.item.inventory.equipment.EquipmentTypes;
+import org.spongepowered.api.item.inventory.property.InventoryDimension;
+import org.spongepowered.api.item.inventory.property.InventoryTitle;
+import org.spongepowered.api.item.inventory.property.SlotIndex;
+import org.spongepowered.api.item.inventory.property.SlotPos;
+import org.spongepowered.api.item.inventory.query.QueryOperationTypes;
+import org.spongepowered.api.item.inventory.type.GridInventory;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.util.annotation.NonnullByDefault;
 
@@ -56,8 +74,8 @@ public class InvSeeCommand extends AbstractCommand<Player> implements Reloadable
 
     @Override
     public CommandElement[] getArguments() {
-        return new CommandElement[] {
-            SelectorWrapperArgument.nicknameSelector(Text.of(player), NicknameArgument.UnderlyingType.USER)
+        return new CommandElement[]{
+                SelectorWrapperArgument.nicknameSelector(Text.of(player), NicknameArgument.UnderlyingType.USER)
         };
     }
 
@@ -74,13 +92,12 @@ public class InvSeeCommand extends AbstractCommand<Player> implements Reloadable
         }
 
         if (permissions.testSuffix(target, "exempt.target", src, false)) {
-            throw new ReturnMessageException(plugin.getMessageProvider().getTextMessageWithFormat("command.invsee.targetexempt", target.getName()));
+            //throw new ReturnMessageException(plugin.getMessageProvider().getTextMessageWithFormat("command.invsee.targetexempt", target.getName()));
         }
 
         // Just in case, get the subject inventory if they are online.
         try {
-            Inventory targetInv = target.isOnline() ? target.getPlayer().get().getInventory() : target.getInventory();
-            Optional<Container> oc = src.openInventory(targetInv);
+            Optional<Container> oc = src.openInventory(getInvSeeInventory(target));
             if (oc.isPresent()) {
                 if (!permissions.testSuffix(src, "modify") || permissions.testSuffix(target, "exempt.interact")) {
                     InvSeeListener.addEntry(src.getUniqueId(), oc.get());
@@ -98,5 +115,38 @@ public class InvSeeCommand extends AbstractCommand<Player> implements Reloadable
     @Override public void onReload() {
         this.self = plugin.getConfigValue(InventoryModule.ID, InventoryConfigAdapter.class,
                 InventoryConfig::isAllowInvseeOnSelf).orElse(false);
+    }
+
+    private Inventory getInvSeeInventory(User user) {
+        final Inventory inventory = Inventory.builder()
+                .of(InventoryArchetypes.DOUBLE_CHEST)
+                .property(InventoryTitle.PROPERTY_NAME, InventoryTitle.of(Text.of(user.getName())))
+                .property(InventoryDimension.PROPERTY_NAME, InventoryDimension.of(9, 5))
+                .build(Nucleus.getNucleus().getPluginContainer());
+        final UserInventory<User> userInventory = user.getPlayer().isPresent()?
+                user.getPlayer().get().getInventory().query(QueryOperationTypes.INVENTORY_TYPE.of(UserInventory.class)) :
+                user.getInventory().query(QueryOperationTypes.INVENTORY_TYPE.of(UserInventory.class));
+
+        int i = 0;
+        for (Inventory slot : inventory.slots()) {
+            if (i < 9) {
+                if (i == 0) {
+                    user.getEquipped(EquipmentTypes.HEADWEAR).ifPresent(slot::set);
+                } else if (i == 1) {
+                    user.getEquipped(EquipmentTypes.CHESTPLATE).ifPresent(slot::set);
+                } else if (i == 2) {
+                    user.getEquipped(EquipmentTypes.LEGGINGS).ifPresent(slot::set);
+                } else if (i == 3) {
+                    user.getEquipped(EquipmentTypes.BOOTS).ifPresent(slot::set);
+                } else if (i == 4) {
+                    user.getEquipped(EquipmentTypes.OFF_HAND).ifPresent(slot::set);
+                }
+            } else {
+                userInventory.getMain().peek(SlotIndex.of(i - 9)).ifPresent(slot::set);
+            }
+            i++;
+        }
+
+        return inventory;
     }
 }
